72ee311e58f43c9c19230e35f8f6dbda61948155
Benjamin Ifeoluwa <152674548+Benjamin-NIG@users.noreply.github.com>
2023-12-05 11:05:40 +0800

    Update func.py

--- utils/func.py
+++ utils/func.py
@@ -2,9 +2,9 @@
 # Import required libraries
 import os
 import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer, BitsAndBytesConfig
 from dotenv import load_dotenv
-
+import streamlit as st
 # Import the required langchain llama index libraries 
 from llama_index.prompts.prompts import SimpleInputPrompt
 from llama_index.llms import HuggingFaceLLM
@@ -12,48 +12,59 @@ from llama_index.embeddings import LangchainEmbedding
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from llama_index import set_global_service_context
 from llama_index import ServiceContext
-from llama_index import VectorStoreIndex, download_loader
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
 from pathlib import Path
 
 # Define variable to hold llama2 weights naming 
-name = "meta-llama/Llama-2-70b-chat-hf"
+name = "meta-llama/Llama-2-7b-chat-hf"
 
 # Set auth token variable from hugging face 
 load_dotenv()
 
 auth_token = os.environ["HUGGING_FACE_API_KEY"]
- 
+bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        bnb_4bit_compute_dtype=torch.bfloat16,
+    )
+
+#to avoid reloading of the model at each run
+@st.cache_resource 
 def get_tokenizer_model(name, auth_token):
     # Create tokenizer
     tokenizer = AutoTokenizer.from_pretrained(name, cache_dir='./model/', use_auth_token=auth_token)
 
-    # Create model
+    # # Create model (some changes here)
+    # model = AutoModelForCausalLM.from_pretrained(name, cache_dir='./model/'
+    #                         , use_auth_token=auth_token, torch_dtype=torch.float16, 
+    #                         rope_scaling={"type": "dynamic", "factor": 2}, load_in_8bit=True)
     model = AutoModelForCausalLM.from_pretrained(name, cache_dir='./model/'
                             , use_auth_token=auth_token, torch_dtype=torch.float16, 
-                            rope_scaling={"type": "dynamic", "factor": 2}, load_in_8bit=True) 
-
+                            quantization_config=bnb_config, device_map='auto') 
     return tokenizer, model
-tokenizer, model = get_tokenizer_model(tokenizer, model)
 
-def llm_query(tokenizer, model):
+# Load the tokenizer and the LLM model
+tokenizer, model = get_tokenizer_model(name, auth_token)
+
+def llm_query():
     # Create a system prompt 
     system_prompt = """<s>[INST] <<SYS>>
     You are a helpful, respectful and honest assistant. Always answer as 
     helpfully as possible, while being safe. Your answers should not include
     any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content.
-    Please ensure that your responses are socially unbiased and positive in nature.
+    Please ensure that your responses are socially unbiased and positive.
 
-    If a question does not make any sense, or is not factually coherent, explain 
+    If a question does not make any sense or is not factually coherent, explain 
     why instead of answering something not correct. If you don't know the answer 
     to a question, please don't share false information.
 
-    Your goal is to provide answers relating to the admmission and research information of the 
-    University.<</SYS>>
+    Your goal is to provide answers relating to the course information document provided.<</SYS>>
     """
     # query prompt wrapper
     query_wrapper_prompt = SimpleInputPrompt("{query_str} [/INST]")
 
-    # Create a HF LLM using the llama index wrapper 
+    # Create an HF LLM using the llama index wrapper 
     llm = HuggingFaceLLM(context_window=4096,
                         max_new_tokens=256,
                         system_prompt=system_prompt,
@@ -77,14 +88,8 @@ def llm_query(tokenizer, model):
   
     #set the service context
     #set_global_service_context(service_context)
-
-    # Download PDF Loader 
-    PyMuPDFReader = download_loader("PyMuPDFReader")
-    # Create PDF Loader
-    loader = PyMuPDFReader()
-    # Load documents 
-    documents = loader.load(file_path=Path('./data/PolyU_StudentHandbook.pdf'), metadata=True)
-
+    documents = SimpleDirectoryReader('./BCN_4787C.pdf').load_data()
+ 
     # Create an index
     index = VectorStoreIndex.from_documents(documents, service_context=service_context)
     # Setup index query engine using LLM 
